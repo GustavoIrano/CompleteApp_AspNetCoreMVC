4d05c61d734d6b3d52ac9e75bfd2b1a67db96c8f
Gustavo Irano <gustavo.irano@hotmail.com>
2020-01-06 15:31:20 -0300

    Complementando um pouco as views

---
+++ src/CPTAPP.APP/Extensions/RazorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CPTAPP.App.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string FormataDocumento(this RazorPage page, int tipoPessoa, String documento)
+        {
+            return tipoPessoa == 1 ? Convert.ToUInt64(documento).ToString(@"000\.000\.000\-00") :
+                Convert.ToUInt64(documento).ToString(@"00\.000\.000\/0000\-00");
+        }
+    }
+}
